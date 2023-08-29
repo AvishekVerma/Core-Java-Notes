2bda321a7d6918a89fcaa428cdacf57ebc32809d
Avishek Verma <avishekverma298@gmail.com>
2023-08-29 19:49:47 +0530

    Chapter - 2 codes completed

---
+++ Code/C-2_A_Trip_to_Objectville/DrumkitTestDrive.java
@@ -0,0 +1,26 @@
+class Drumkit{
+	boolean topHat = true;
+	boolean snare = true;
+
+	void playSnare(){
+		System.out.println("bang bang ba-bang");
+	}
+
+	void playTopHat(){
+		System.out.println("ding ding do-ding");
+	}
+}
+
+class DrumkitTestDrive{
+	public static void main(String[] args){
+		Drumkit d = new Drumkit();
+		d.playSnare();
+		d.snare = false;
+		d.playTopHat();
+
+		if(d.snare == true){
+			d.playSnare();
+		}
+
+	}
+}
---
+++ Code/C-2_A_Trip_to_Objectville/Square.java
@@ -0,0 +1,8 @@
+public Square{
+	void rotate(){
+		System.out.println("Square got rotated...");
+	}
+	void playSound(){
+		System.out.println("Played the sound for : Square...");
+	}
+}
